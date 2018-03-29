ceb853648066934fe098fb62d2f49baa5f881faf
Marco-hui <matao_1891@163.com>
2018-03-29 20:04:39 +0800

    删除多余文件

--- api/db/index.js
+++
@@ -1,5 +0,0 @@
-var DBHelper =require("./DBHelper")
-
-module.exports={
-    DBHelper
-}
--- api/utils/apiResult.js
+++
@@ -1,3 +0,0 @@
-module.exports=function(status,data,error,message){
-    return {status,data,error,message};
-}
